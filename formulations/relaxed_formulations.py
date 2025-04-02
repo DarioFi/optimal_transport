c684664b0d87595df8b6638b6111d8b7106c0ece
DarioFi <filatrella.dario@gmail.com>
2025-04-02 16:49:24 +0200

    chore: sync

--- formulations/relaxed_formulations.py
+++ formulations/relaxed_formulations.py
@@ -70,7 +70,31 @@ def add_bilinear(model, z, x, y, index_sets, relaxed: bool, constraint_name: str
         model.add_component(constraint_name + "_4", pyo.Constraint(index_wrapped, rule=bilinear_constraint_rule_4))
 
 
-def dbt_relaxed_alpha0(terminals, alpha, masses, relax_y: bool, relax_w: bool, disjunctive_w: bool, use_geometric_cut_50: bool):
+def enumerate_assignments(A, B):
+    """
+    Enumerate all possible functions A -> B
+    :return:
+    """
+    fs = [{
+        a: b for a in A for b in b_assignemnt
+    } for b_assignemnt in itertools.product(B, repeat=len(A))]
+    return [pyomo_wrapper(f) for f in fs]
+    # return [pyomo_wrapper(x) for x in itertools.product(range(P), repeat=S)]
+
+
+def dbt_relaxed_alpha0(terminals, alpha, masses, relax_y: bool, relax_w: bool, disjunctive_w: int | bool,
+                       use_geometric_cut_50: bool):
+    """
+
+    :param terminals:
+    :param alpha:
+    :param masses:
+    :param relax_y:
+    :param relax_w:
+    :param disjunctive_w: how many x variables to put in the disjunctive assignemnt
+    :param use_geometric_cut_50:
+    :return:
+    """
     assert alpha == 0
     P = len(terminals)
     S = len(terminals) - 2
@@ -121,29 +145,77 @@ def dbt_relaxed_alpha0(terminals, alpha, masses, relax_y: bool, relax_w: bool, d
 
     # region X-polytope
 
-    model.cc = pyo.Var(model.S, model.P, domain=pyo.NonNegativeReals, bounds=(0, 1))
+    if disjunctive_w:
+        # first disjunctive_w elements of model.s
+
+        disjunctive_s = [x for i, x in enumerate(model.S) if i < disjunctive_w]
+        classic_s = [x for i, x in enumerate(model.S) if i >= disjunctive_w]
+
+        if len(classic_s) > 0:
+            classic_s = pyo.RangeSet(min(classic_s), max(classic_s))
+
+        disjunctive_s = pyo.RangeSet(min(disjunctive_s), max(disjunctive_s))
+
+        model.A = enumerate_assignments(disjunctive_s, model.P)
+    else:
+        disjunctive_s = []
+        classic_s = model.S
+
+    model.cc = pyo.Var(classic_s, model.P, domain=pyo.NonNegativeReals, bounds=(0, 1))
 
     def convex_hull_constraint(model, i, d):
         return sum(terminals[j][d] * model.cc[i, j] for j in model.P) == model.x[i, d]
 
-    model.convex_hull_constraint = pyo.Constraint(model.S, model.D, rule=convex_hull_constraint)
+    model.convex_hull_constraint = pyo.Constraint(classic_s, model.D, rule=convex_hull_constraint)
+
+    if disjunctive_w:
+        model.x_a = pyo.Var(model.A, disjunctive_s, model.D, domain=pyo.NonNegativeReals, bounds=(0, 1))
+
+        model.l_a = pyo.Var(model.A, domain=pyo.NonNegativeReals, bounds=(0, 1))
+
+        def disj_sum_x_constraint(model, d, i):
+            return sum(model.x_a[a, i, d] for a in model.A) == model.x[i, d]
+
+        model.disj_sum_constraint = pyo.Constraint(model.D, disjunctive_s, rule=disj_sum_x_constraint)
+
+        def disj_assignment_constraint(model, a, i, d):
+            return model.x_a[a, i, d] == terminals[a.data[i]][d] * model.l_a[a]
+
+        model.disj_assignment_constraint = pyo.Constraint(model.A, disjunctive_s, model.D,
+                                                          rule=disj_assignment_constraint)
+
+        def sum_l_constraint(model):
+            return sum(model.l_a[a] for a in model.A) == 1
+
+        model.sum_l_constraint = pyo.Constraint(rule=sum_l_constraint)
+
+        # todo finish the b_a constraints
 
     # endregion
 
     # region bilinear-terms
+
+    # some w are unaffected by the disjunctive relaxation, those who do not depend on disjunctive_s
+    # in particular those which depend on terminals are always unaffected
+    # the rest are affected and need to be split into two parts
+
+    #
 
     w_index = [(i, j) for i in model.S for j in model.S if i != j] + [(i, j) for i in model.S for j in model.P] + [
         (j, i) for i in
         model.S for j
         in model.P]
 
+    # this stays the same even when using the disjunctive version
     model.w = pyo.Var(w_index, model.D, domain=pyo.NonNegativeReals, bounds=(0, 1))
 
-    w_index_s_s = [
+    # if (i,d) is in the disjunctive_s then we need to apply it to w_a
+    w_index_s_s_classic = [
         ((((i, j), d), (i, d), (min(i, j), max(i, j)))) for i in model.S for j in model.S for d in model.D if
-        i != j
+        i != j and i in classic_s
     ]
-    add_bilinear(model, model.w, model.x, model.y, w_index_s_s, relax_w, "w_bilinear_constraint_s_s")
+
+    add_bilinear(model, model.w, model.x, model.y, w_index_s_s_classic, relax_w, "w_bilinear_constraint_s_s")
 
     w_index_p_s = [
         ((((i, j), d), (i, d), (min(i, j), max(i, j)))) for i in model.P for j in model.S for d in model.D
@@ -152,14 +224,46 @@ def dbt_relaxed_alpha0(terminals, alpha, masses, relax_y: bool, relax_w: bool, d
     # note: this is never relaxed because terminals are constant and therefore this constraint is actually linear
     add_bilinear(model, model.w, terminals_dict, model.y, w_index_p_s, False, "w_bilinear_constraint_p_s")
 
-    w_index_s_p = [
+    # if (i,d) is in the disjunctive_s then we need to apply it to w_a
+    w_index_s_p_classic = [
         ((((i, j), d), (i, d), (min(i, j), max(i, j)))) for i in model.S for j in model.P for d in model.D
+        if i in classic_s
     ]
-    add_bilinear(model, model.w, model.x, model.y, w_index_s_p, relax_w, "w_bilinear_constraint_s_p")
+    # todo: we should assert that min(i,j) = j and max(i,j) = i
+
+    add_bilinear(model, model.w, model.x, model.y, w_index_s_p_classic, relax_w, "w_bilinear_constraint_s_p")
+
+    if disjunctive_w:
+        w_index_disjunctive = [
+            ((i, j), d) for i in disjunctive_s for j in model.S.union(model.P) for d in model.D
+            if i != j
+        ]
+
+        w_index_disjunctive_wrap = [pyomo_wrapper(x) for x in w_index_disjunctive]
+
+        w_index_disjunctive_2 = [(x, d) for x in w_index for d in model.D if x[0] in disjunctive_s]
+
+        assert sorted(list(w_index_disjunctive)) == sorted(list(w_index_disjunctive_2))
+
+        print("Assertion passed")
+
+        model.w_a = pyo.Var(model.A, w_index_disjunctive, domain=pyo.NonNegativeReals, bounds=(0, 1))
+
+        def disj_sum_w_constraint(model, w_ind):
+            return sum(model.w_a[a, w_ind.data] for a in model.A) == model.w[w_ind.data]
+
+        model.disj_sum_w_constraint = pyo.Constraint(w_index_disjunctive_wrap, rule=disj_sum_w_constraint)
+
+        def w_bilinear_disj_constraint(model, a, w_ind):
+            i, j = w_ind.data[0]
+            d = w_ind.data[1]
+            return model.w_a[a, (i, j), d] == model.l_a[a] * model.y[min(i, j), max(i, j)] * terminals_dict[
+                (a.data[i], d)]
+
+        model.w_bilinear_disj_constraint = pyo.Constraint(model.A, w_index_disjunctive_wrap,
+                                                          rule=w_bilinear_disj_constraint)
 
     # endregion
-
-    # objective function:
 
     def objective_rule(model):
         return sum(
@@ -196,11 +300,10 @@ if __name__ == '__main__':
         [[0, 1], [0, 0], [1, 0], [1, 1]],
         masses=[0, 0, 0, 0],
         alpha=0,
-        relax_w=True,
+        relax_w=False,
         relax_y=False,
-        disjunctive_w=False,
+        disjunctive_w=2,
         use_geometric_cut_50=False
     )
 
     m.pprint()
-
--- test_correctedness_relaxation.py
+++ test_correctedness_relaxation.py
@@ -9,7 +9,7 @@ n = 5
 # solver_opt = "maxiter=1 maxtime=300 LBTTDo=0 "
 # solver_opt = "maxiter=1 maxtime=300 TDo=0 MDo=0 LBTTDo=0 OBTTDO=0 PDo=0"
 
-solver_opt = "maxtime=60"
+solver_opt = "maxtime=60 EpsA=1e-5"
 
 print("Running full solutions no cut")
 exp = Experiment(
@@ -26,8 +26,8 @@ exp = Experiment(
     formulation_arguments={
         'relax_y': False,
         'relax_w': False,
-        'disjunctive_w': False,
-        'use_geometric_cut_50': False
+        'disjunctive_w': 3,
+        'use_geometric_cut_50': True
     }
 )
 
@@ -64,13 +64,16 @@ for i in range(n_runs):
     t_no_cut = data_no_cut[i]['results']['termination_condition']
     t_cut = data_cut[i]['results']['termination_condition']
     if t_no_cut != t_cut:
+        print("-----")
         print(f"{t_no_cut} != {t_cut}")
         print(f"{data_no_cut[i]['results']['lower_bound']} == {data_cut[i]['results']['lower_bound']}")
         print(f"{data_no_cut[i]['results']['upper_bound']} == {data_cut[i]['results']['upper_bound']}")
+        print("-----")
     else:
         print(f"{data_no_cut[i]['results']['lower_bound']} == {data_cut[i]['results']['lower_bound']}")
-        assert abs(data_no_cut[i]['results']['lower_bound'] - data_cut[i]['results']['lower_bound']) < 1e-5
-        assert abs(data_no_cut[i]['results']['upper_bound'] - data_cut[i]['results']['upper_bound']) < 1e-5
+        if abs(data_no_cut[i]['results']['lower_bound'] - data_cut[i]['results']['lower_bound']) > 1e-5 or \
+            abs(data_no_cut[i]['results']['upper_bound'] - data_cut[i]['results']['upper_bound']) > 1e-5:
+            print("Wrong!")
 
 
 print(f"Average time no cut: {sum([x['results']['time'] for x in data_no_cut]) / n_runs}")
